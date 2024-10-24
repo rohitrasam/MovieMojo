00460a375190e502ad73a76500d22a2cef0fd70a
Rohit Rasam <rasamrohit@gmail.com>
2024-10-24 12:03:27 +0530

    get total users

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("signup", user_signup, name="user-signup"),
     path("login", user_login, name="user-login"),
     path("reset_password", reset_password, name="reset-password"),
+    path('total_users', get_total_users, name='get-total-users')
 
 ]
