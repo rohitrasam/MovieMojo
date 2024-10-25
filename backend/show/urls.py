ea1ac1070430ff7f4753dcec1b5fd60758ee8dd7
Rohit Rasam <rasamrohit@gmail.com>
2024-10-25 16:48:02 +0530

    implemented show functionality

--- backend/show/urls.py
+++ backend/show/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path("add_show", view=add_show, name='add-show'),
+    path("get_admin_shows", view=get_admin_shows, name="get-admin-shows"),
+    path("get_home_shows", view=get_home_shows, name="get-home-shows"),
 ]
