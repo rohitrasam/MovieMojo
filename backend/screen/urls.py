fd4e6359f196e098e660df2d98766aefb06e7635
Rohit Rasam <rasamrohit@gmail.com>
2024-12-10 20:53:03 +0530

    seat and screen update func

--- backend/screen/urls.py
+++ backend/screen/urls.py
@@ -9,8 +9,6 @@ urlpatterns = [
     path('delete/<int:id>', view=delete_screen, name='delete-screen'),
     path('get_seats', view=get_seats, name='get-seats'),
     path('update_seats', view=update_seats, name='update-seats'),
-    path('assign_seat_type', view=assign_seat_type, name='assign_seat_type'),
-    path('get_seat_assignments', view=get_seat_assignments, name='get_seat_assignments'),
     path("get_total_screens", view=get_total_screens, name="get-home-shows"),
 
 ]
