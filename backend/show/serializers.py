f0cde74e41f5e63c188d83231a8b8ba5506b8097
Rohit Rasam <rasamrohit@gmail.com>
2024-12-16 23:00:11 +0530

    booking functionality

--- backend/show/serializers.py
+++ backend/show/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from theatre.serializers import TheatreSerializer
 from movie.serializers import MovieSerializer
 from screen.serializers import ScreenSerializer
-from models.models import Show, Booking
+from models.models import Seat, Show, Booking
 
 class AdminShowSerializer(serializers.ModelSerializer):
 
@@ -33,3 +33,26 @@ class HomeShowSerializer(serializers.ModelSerializer):
     
     def get_movie(self, obj: Show):
         return MovieSerializer(obj.movie).data
+    
+class UserBookingSerializer(serializers.ModelSerializer):
+
+    time = serializers.SerializerMethodField()
+    seats = serializers.SerializerMethodField()
+    # user = serializers.SerializerMethodField()
+    # show = serializers.SerializerMethodField()
+    class Meta:
+        model = Booking
+        fields = '__all__'
+    
+    def get_time(self, obj: Booking):
+        print(obj)
+        return obj.show.time
+    
+    def get_seats(self, obj: Booking):
+        return [booking.seat.seat_num for booking in Booking.objects.filter(user=obj.user)]
+
+    # def get_user(self, obj: Booking):
+    #     pass
+
+    # def get_show(self, obj: Booking):
+    #     pass
